8b7b8e93e3e59f0fbcae8d74113f7615822de021
munazzabegam <4pa21ec037@pace.edu.in>
2024-02-07 19:37:46 +0530

    updated

--- db.sql
+++ db.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Thasbeeh (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
+    email VARCHAR(255),
     UniqueId VARCHAR(100) UNIQUE,
     Thasbeeh_count INT DEFAULT 0
 );
